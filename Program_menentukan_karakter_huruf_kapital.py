67f777e5c109cd49320a1e0230eff7118ee21f8c
kevinardhana <143370449+kevinardhana@users.noreply.github.com>
2023-09-11 12:18:20 +0800

    Add files via upload

---
+++ Program_menentukan_karakter_huruf_kapital.py
@@ -0,0 +1,9 @@
+Karakter = input("Karakter = ")
+
+x = Karakter.isupper()
+y = Karakter.islower()
+z = Karakter.isdigit()
+
+print(f'Huruf kapital? {x}')
+print(f'Huruf kecil? {y}')
+print(f'Angka? {z}')
